3b91bb8b33c4f32d3e96fbe4d3aac973753ee36d
ljl <lxchw@qq.com>
2024-09-23 18:27:24 +0800

    Finished PA0 and some commands(si, info)

---
+++ nemu/src/isa/riscv32/reg.c
@@ -0,0 +1,42 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <isa.h>
+#include "local-include/reg.h"
+
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+#define NR_REGS ARRLEN(regs)
+
+void isa_reg_display() {
+	/* add my codes */
+	int i=0;
+	for(;i<NR_REGS;i++)
+	{
+		printf("%3s:%x ",regs[i],*regs[i]);
+		if(i%8==7)
+			printf("\n");
+	}
+	/* add my codes */
+}
+
+word_t isa_reg_str2val(const char *s, bool *success) {
+  return 0;
+}
